4bcd4694b19be84f71569225615a26c44135d9a7
NTiger07 <falaleru@gmail.com>
2025-05-30 07:35:30 +0100

    Added StorageFactory

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -1,4 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
+import {SimpleStorage} from "./SimpleStorage.sol";
 
-contract StorageFactory {}
+contract StorageFactory {
+    SimpleStorage public SimpleStorage;
+
+    function constructSimpleStorageContract() public {
+        simpleStorage = new SimpleStorage();
+    }
+}
