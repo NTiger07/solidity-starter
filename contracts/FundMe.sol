d95dde95c3c9eb9eeebfd7e65a0c9ac96e977ed9
NTiger07 <falaleru@gmail.com>
2025-06-06 15:33:20 +0100

    FundMe DONE

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,20 +1,42 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.1;
 import {PriceConverter} from "./PriceConverter.sol";
+error NotOwner();
 
 contract FundMe {
     using PriceConverter for uint256;
-    uint256 minimumUSD = 5e18;
+    uint256 public constant MINIMUM_USD = 5e18;
+    address public immutable i_owner;
     address[] funders;
     mapping(address funder => uint amountFunded) public addressToAmountFunded;
 
+    modifier onlyOwner() {
+        if (msg.sender != i_owner) {
+            revert NotOwner();
+        }
+        // require(msg.sender == i_owner, "Only owner can call this function");
+        _;
+    }
+
+    constructor() {
+        i_owner = msg.sender; //msg.sender is the address that deploys the contract
+    }
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
+
     function fund() public payable {
-        require(msg.value.getConversionRate() >= minimumUSD, "Too less");
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "Too less");
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value;
     }
 
-    function withdraw() public {
+    function withdraw() public onlyOwner {
         for (
             uint funderIndex = 0;
             funderIndex < funders.length;
@@ -33,7 +55,9 @@ contract FundMe {
         // require(sendSuccess, "Send failed"); //only reverts if require fails
 
         //call
-        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
         require(callSuccess, "Call failed"); //only reverts if require fails
     }
 }
